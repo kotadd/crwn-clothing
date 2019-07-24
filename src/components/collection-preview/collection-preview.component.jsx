d523ddf7cf9945d5ff39984e2a8bb33dc2371fd2
Kota Nishinaka <private2030kota@gmail.com>
2019-07-25 00:39:47 +0900

    Fix bug

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,12 +7,13 @@ import './collection-preview.styles.scss';
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview" />
-    {items
-      .filter((item, idx) => idx < 4)
-      .map(({ id, ...otherItemProps }) => (
-        <CollectionItem key={id} {...otherItemProps} />
-      ))}
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
   </div>
 );
 
