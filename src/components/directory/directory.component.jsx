162c1c0710e57c687253440da191139f0ddc7af6
Kota Nishinaka <private2030kota@gmail.com>
2019-08-05 00:28:07 +0900

    added styled-components for custom-button, header and homepage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
 
 const Directory = ({ sections }) => {
   return (
