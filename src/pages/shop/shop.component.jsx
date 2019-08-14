88ede590a266c5768ae23212f9b3b39dcde23ef8
Kota Nishinaka <private2030kota@gmail.com>
2019-08-14 22:59:10 +0900

    Add promise pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,16 +28,11 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    fetch(
-      'https://firestore.googleapis.com/v1/projects/crwn-db-d6ba9/databases/(default)/documents/cities/LA'
-    )
-      .then(response => response.json())
-      .then(collections => console.log(collections));
-    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-    //   updateCollections(collectionsMap);
-    //   this.setState({ loading: false });
-    // });
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   render() {
